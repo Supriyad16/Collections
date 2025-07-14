11535ce899f817e45b38fef6aee1fb2cbc9f8996
Supriya D <dinakaranctsupriya@gmail.com>
2025-07-14 23:44:50 +0530

    HashMap

---
+++ doll/src/com/xworkz/Map/Cartoon.java
@@ -0,0 +1,26 @@
+package com.xworkz.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cartoon {
+
+    public static void main(String[] args) {
+        Map<String, String> cartoon = new HashMap<>();
+
+        cartoon.put("Dora","Boots");
+        cartoon.put("Bheem","Chutki");
+        cartoon.put("Doremon", "Nobita");
+        cartoon.put("Shinchan","Shiro");
+        cartoon.put("Oggy", "Jack");
+        cartoon.put("Tom","Jerry");
+        cartoon.put("Mr.Bean","Teddy");
+        cartoon.put("Peppa Pig","Rabbit");
+        cartoon.put("Masha","Bear");
+        cartoon.put("Motu","Patlu");
+
+        System.out.println(cartoon.get("Dora"));
+
+    }
+
+}
